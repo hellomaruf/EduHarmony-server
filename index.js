47cea991f236bf69f08304a65593a68139b3cb30
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-01 14:56:47 +0600

    Added users in mongodb

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.0o9qayn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//middleware
+app.use(express.json());
+app.use(cors());
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.0o9qayn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,7 +21,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const usersCollection = client.db("EduHarmony").collection("users");
+
     // Connect the client to the server	(optional starting in v4.7)
+
+    // Added user in database as a student
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const query = { email: users?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User is already Exist", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
