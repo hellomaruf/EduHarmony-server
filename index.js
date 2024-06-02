7e5c4c7e3742682c0b2587418b4c528b9d141f29
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-02 13:21:02 +0600

    Get all user

--- index.js
+++ index.js
@@ -59,13 +59,19 @@ async function run() {
       res.send(result);
     });
 
-      //Get user role by email
-      app.get("/user/:email", async (req, res) => {
-        const email = req.params.email;
-        const result = await usersCollection.findOne({ email });
-        res.send(result);
-      });
-    
+    //Get user role by email
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
